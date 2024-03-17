4f7b78dbc414368af34c1af6d16a48bd51662d40
hichamecode <hichame.code@gmail.com>
2024-03-17 12:57:44 +0100

    bug hunt Dashboard - corrected

--- Billed-app-FR-Front/src/containers/Dashboard.js
+++ Billed-app-FR-Front/src/containers/Dashboard.js
@@ -85,30 +85,30 @@ export default class {
     if (typeof $('#modaleFileAdmin1').modal === 'function') $('#modaleFileAdmin1').modal('show')
   }
 
-  handleEditTicket(e, bill, bills) {
-    if (this.counter === undefined || this.id !== bill.id) this.counter = 0
-    if (this.id === undefined || this.id !== bill.id) this.id = bill.id
-    if (this.counter % 2 === 0) {
-      bills.forEach(b => {
-        $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
-      })
-      $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
-      $('.dashboard-right-container div').html(DashboardFormUI(bill))
-      $('.vertical-navbar').css({ height: '150vh' })
-      this.counter ++
-    } else {
-      $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
+// on crée un boolean au lieu de counter
+handleEditTicket(e, bill, bills) {
+  if (this.clicked === undefined || this.id !== bill.id) this.clicked = false
+  this.id = bill.id
+  if (!this.clicked) {
+    bills.forEach((b) => {
+      $(`#open-bill${b.id}`).css({ background: '#0D5AE5' });
+    });
+    $(`#open-bill${bill.id}`).css({ background: '#2A2B35' });
+    $('.dashboard-right-container div').html(DashboardFormUI(bill));
+    $('.vertical-navbar').css({ height: '150vh' });
+  } else {
+    $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' });
 
-      $('.dashboard-right-container div').html(`
-        <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
-      `)
-      $('.vertical-navbar').css({ height: '120vh' })
-      this.counter ++
-    }
-    $('#icon-eye-d').click(this.handleClickIconEye)
-    $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
-    $('#btn-refuse-bill').click((e) => this.handleRefuseSubmit(e, bill))
+    $('.dashboard-right-container div').html(`
+      <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
+    `);
+    $('.vertical-navbar').css({ height: '120vh' });
   }
+  this.clicked = !this.clicked;
+  $('#icon-eye-d').click(this.handleClickIconEye);
+  $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill));
+  $('#btn-refuse-bill').click((e) => this.handleRefuseSubmit(e, bill));
+}
 
   handleAcceptSubmit = (e, bill) => {
     const newBill = {
@@ -131,26 +131,26 @@ export default class {
   }
 
   handleShowTickets(e, bills, index) {
-    if (this.counter === undefined || this.index !== index) this.counter = 0
-    if (this.index === undefined || this.index !== index) this.index = index
-    if (this.counter % 2 === 0) {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html(cards(filteredBills(bills, getStatus(this.index))))
-      this.counter ++
+    if (this.clicked === undefined || this.index !== index) this.clicked = false;
+    this.index = index;
+    if (!this.clicked) {
+      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)' });
+      $(`#status-bills-container${this.index}`).html(
+        cards(filteredBills(bills, getStatus(this.index)))
+      );
     } else {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html("")
-      this.counter ++
+      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)' });
+      $(`#status-bills-container${this.index}`).html('');
     }
+    this.clicked = !this.clicked;
+    // add event listener only on filtered bills
+    filteredBills(bills, getStatus(this.index)).forEach((bill) => {
+      $(`#open-bill${bill.id}`).click((e) =>
+        this.handleEditTicket(e, bill, bills)
+      );
+    });
 
-    bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
-    })
-
-    return bills
-
+    return bills;
   }
 
   getBillsAllUsers = () => {
