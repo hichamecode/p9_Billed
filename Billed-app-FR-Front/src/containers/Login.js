7a269ec2dbc658edb1abc23fc836e6c599891431
hichamecode <hichame.code@gmail.com>
2024-03-17 12:43:50 +0100

    bug report Login - corrected

--- Billed-app-FR-Front/src/containers/Login.js
+++ Billed-app-FR-Front/src/containers/Login.js
@@ -37,12 +37,13 @@ export default class Login {
 
   }
 
+  // correction du querySelector sur le parcours Admin
   handleSubmitAdmin = e => {
     e.preventDefault()
     const user = {
       type: "Admin",
-      email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
-      password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+      email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
+      password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
       status: "connected"
     }
     this.localStorage.setItem("user", JSON.stringify(user))
